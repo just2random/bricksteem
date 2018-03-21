5c1f3c32b117d292bea9bfc289ec3e680272cda9
just2random <36524020+just2random@users.noreply.github.com>
2018-03-21 20:56:53 +1100

    Change Site Name

--- src/client/components/Navigation/Topnav.js
+++ src/client/components/Navigation/Topnav.js
@@ -345,7 +345,7 @@ class Topnav extends React.Component {
           <div className={classNames('left', { 'Topnav__mobile-hidden': searchBarActive })}>
             <Link className="Topnav__brand" to="/">
               <i className="iconfont icon-busy Topnav__brand-icon" />
-              busy
+              bricksteem
             </Link>
             <span className="Topnav__version">beta</span>
           </div>
